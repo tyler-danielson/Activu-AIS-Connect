a4bd52df71e85f34bad7d2119df745f05dabd52b
tyler-danielson <38443137+tyler-danielson@users.noreply.github.com>
2022-06-21 10:09:26 -0600

    Add files via upload

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Activu AIS Connect",
   Version = "0.9",
-  BuildVersion = "1.0.0.127",
+  BuildVersion = "1.0.0.128",
   Id = "Activu Connection Block",
   Author = "Activu",
   Description = "A plugin to interface with Activu Interface Server.  Written by Tyler Danielson for Activu Corp."  
